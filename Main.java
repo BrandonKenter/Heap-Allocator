2db88c1113a6b67076f451d362667fc0ae2fd9da
Brandon Kenter <bjkenter@wisc.edu>
2021-01-12 19:43:42 -0600

    added comment

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package sample;
+package allocator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,16 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main class to initialize and start the application.
+ *
+ * @author Brandon Kenter
+ */
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("screens/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("screens/MainScreen.fxml"));
         primaryStage.setTitle("Heap Allocator");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
